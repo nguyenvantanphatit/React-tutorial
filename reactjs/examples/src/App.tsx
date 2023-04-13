b66f33081f5afe5a8621c80d42fdbe6809fbc891
lammas-laptrinh <danghoangson51@gmail.com>
2023-04-13 12:23:47 +0700

    update example and readme

--- reactjs/examples/src/App.tsx
+++ reactjs/examples/src/App.tsx
@@ -2,10 +2,12 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import SideBar from "./components/SideBar";
+import ClassComponents from "./views/LifeCycle/ClassComponents";
+import FunctionComponents from "./views/LifeCycle/FunctionComponents";
 import "./App.css";
 
 function App() {
-  return <SideBar />;
+  return <FunctionComponents />;
 }
 
 export default App;
